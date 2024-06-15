cb981891bdc3656d7d125692b599e7ea3dd17b55
hiroshi nagao <hi_1205@hotmail.com>
2024-06-15 12:02:34 +0900

    apiKey issue has not been solved yet

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput  = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "";
+
+const apiKey = "93440e37db0c76647a32275988a0e582";
+
 
 weatherForm.addEventListener("submit", async event => {
     event.preventDefault();
@@ -45,19 +48,42 @@ function displayWeatherInfo(data){
 
     cityDisplay.textContent = city;
     tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°c`;
-    humidityDisplay.textContent = `Humidity: ${humidity}`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
 
     cityDisplay.classList.add("cityDisplay");
     tempDisplay.classList.add("tempDisplay");
     humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
 
     card.appendChild(cityDisplay);
     card.appendChild(tempDisplay);
     card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
 }
 
 function getWeatherEmoji(weatherId){
-
+    switch (true) {
+      case (weatherId >= 200 && weatherId < 300):
+          return "⛈️"
+      case (weatherId >= 300 && weatherId < 400):
+          return "🌧️"
+      case (weatherId >= 500 && weatherId < 600):
+            return "🌨️"
+      case (weatherId >= 600 && weatherId < 700):
+            return "❄️"
+      case (weatherId >= 700 && weatherId < 800):
+            return "🌫️"
+      case (weatherId === 800):
+            return "☀️"
+      case (weatherId >= 801 && weatherId < 98100):
+            return "☁️"
+      default:
+        return "?";
+    }
 }
 
 function dsiplayError(message){
